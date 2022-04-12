a2db205d839080dc01eca9f66ccd3b347c80e3bc
ashment <gundamme@gmail.com>
2022-04-12 02:22:21 -0400

    WIP controller

--- verilog/arr_controller.v
+++ verilog/arr_controller.v
@@ -5,7 +5,7 @@ module ROW_IN_BUF
         input [WORD_LEN:0] in_dat,
     )
 
-
+    // WIP
 
 endmodule
 
@@ -18,6 +18,7 @@ module ARR_CTRL
 
     input clk,
     input rstn,
+    input fire,
 
     input [2:0] mode,
     // mode is used to control current operating mode.
@@ -51,7 +52,7 @@ module ARR_CTRL
     // Total Convs: (w-2) * (cr-2)
 
     reg [3:0] cur_conf = 0;
-    reg [15:0] cur_addr = 0;
+    reg [14:0] cur_addr = 0;
 
     always @ (posedge clk) begin
         if(!rstn) begin
@@ -59,26 +60,31 @@ module ARR_CTRL
             for(i=0; i<4; i=i+1) begin
                configs[i] <= 0; 
             end
-            for(i=0; i<4; i=i+1) begin
-                cur_conf[i] <= 0;
-            end
-            for(i=0; i<16; i=i+1) begin
-                cur_addr[i] <= 0;
-            end
-            inpCEN <= 0;
+            cur_conf[i] <= 0;
+            cur_addr[i] <= 0;
+            inpCEN <= 0; // Mem always on
             inpWEN <= 1;
         end
         else begin
-
-            if(config_load) begin
-                configs[cur_conf] <= data_in;
-            end 
-            if (data_load) begin
-                inpWEN <= 0; // Enable write to inp mem
-            end else begin
-                inpWEN <= 1;
+            case (mode)
+                2'b00: begin
+                    inpWEN <= 1;
+                    configs[cur_conf] <= data_in;
+                end
+                2'b01: begin
+                    inpWEN <= 0; // Enable write to inp mem
+                    cur_addr <= cur_addr + 1;
+                end
+                2'b10: begin
+                    inpWEN <= 1;
+                end
+                default: begin
+                    inpWEN <= 1;
+                end
             end
         end
     end
+
+
 
 endmodule
