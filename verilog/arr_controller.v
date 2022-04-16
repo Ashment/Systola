04d09695b90b43feccb9ebfbc1c2069beb0c1d3b
David Hou <gundamme@gmail.com>
2022-04-15 20:08:38 -0400

    Parameterized mems for controller

--- verilog/arr_controller.v
+++ verilog/arr_controller.v
@@ -1,14 +1,64 @@
 module ROW_IN_BUF
-    #(parameter WORD_LEN = 8)
-    (
+    #(
+        parameter WORDLEN = 8,
+        parameter BUFSIZE = 8
+    )(
         input clk,
-        input [WORD_LEN:0] in_dat,
-    )
+        input rstn,
+        input read,
+        input write,
+        input [WORDLEN-1 : 0] in_dat,
+        output [WORDLEN-1 : 0]
+    );
 
-    // WIP
+    reg [WORDLEN-1 : 0] bufdat [0:BUFSIZE];
+    reg [WORDLEN-1 : 0] outdat;
+    reg [7:0] curhead;
+    reg [7:0] curtail;
 
+    always @ (posedge clk) begin
+        if(!rstn) begin
+            for(i=0; i<BUFSIZE; i=i+1) begin
+               bufdat[i] <= 0; 
+            end
+            outdat <= 0;
+            curhead <= 0;
+            curtail <= 0;
+        end else begin
+            if (read) begin
+
+            end
+        end
+    end
+    
 endmodule
 
+module CLK_DIV
+    #(parameter DIV_CNT = 16, parameter BITS = 4;
+    )(
+        input clk,
+        input rstn,
+        output clkout,
+    );
+
+    reg [BITS-1:0] cnt;
+    reg clkreg;
+    assign clkout = clkreg;
+
+    always @ (posedge clk) begin
+        if(!rstn) begin
+            clkreg <=  0;
+            cnt <= 0;
+        end else begin
+            if(cnt == DIV_CNT-1) begin
+                clkreg <= ~clkreg;
+                cnt <= 0;
+            end else begin
+                cnt <= cnt + 1;
+            end
+        end
+    end
+endmodule
 
 module ARR_CTRL
     #(
@@ -32,6 +82,7 @@ module ARR_CTRL
     input [7:0] data_in,
 
     output valid,
+    output fire,
     output done);
 
     // Instantiate input mem
@@ -39,20 +90,20 @@ module ARR_CTRL
 
     integer i;
     reg [7:0] configs [0:7];
-    reg inpWEN;
-    reg inpCEN;
     // CONFIGURATIONS:
     // 0: operation
     // 1: input Channels
     // 2: input width
     // 3: input height
+    reg inpWEN;
+    reg inpCEN;
 
     // Target Address:
     // Convs per row: cr = w - 2
     // Total Convs: (w-2) * (cr-2)
 
     reg [3:0] cur_conf = 0;
-    reg [14:0] cur_addr = 0;
+    reg [15:0] cur_addr = 0;
 
     always @ (posedge clk) begin
         if(!rstn) begin
@@ -60,23 +111,28 @@ module ARR_CTRL
             for(i=0; i<4; i=i+1) begin
                configs[i] <= 0; 
             end
-            cur_conf[i] <= 0;
-            cur_addr[i] <= 0;
+            cur_conf <= 0;
+            cur_addr <= 0;
             inpCEN <= 0; // Mem always on
             inpWEN <= 1;
         end
         else begin
             case (mode)
-                2'b00: begin
+                2'b00: begin    // MODE: Config Loading
                     inpWEN <= 1;
                     configs[cur_conf] <= data_in;
                 end
-                2'b01: begin
-                    inpWEN <= 0; // Enable write to inp mem
+                2'b01: begin    // MODE: Data Loading
+                    inpWEN <= 0; // Enable write to inpmem iff mode = 01
                     cur_addr <= cur_addr + 1;
                 end
-                2'b10: begin
+                2'b10: begin    // MODE: Computing
                     inpWEN <= 1;
+                    cur_addr <= 0;
+
+                    // Send appropriate data to each PE row
+                    
+
                 end
                 default: begin
                     inpWEN <= 1;
