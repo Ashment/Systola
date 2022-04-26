ada023fc72c7bb190286268b6a4c8a8a99c96b09
ashment <gundamme@gmail.com>
2022-04-26 16:50:04 -0400

    Fixes to rtl pe and pe_arr

--- rtl/pe.sv
+++ rtl/pe.sv
@@ -11,7 +11,7 @@ module PE #(
 	output reg out_f,
 	output reg [7:0] out_a,
 	output reg [7:0] out_w,
-	output reg [OUTWIDTH:0] out_r,
+	output reg [OUTWIDTH-1 : 0] out_r,
 	output reg resvalid);
 	
 	reg fireprev;
@@ -28,6 +28,7 @@ module PE #(
 			out_f <= fire;
 			
 			if (fireprev && !fire) begin
+				// Result is valid when fire goes low after sequence of fire highs.
 				resvalid <= 1;
 			end
 
@@ -37,7 +38,7 @@ module PE #(
 			end
 
 			if (fire) begin
-				if (fireprev <= 0) begin
+				if (!fireprev) begin
 					out_r <= (in_w * in_a);
 				end else begin
 					out_r <= out_r + (in_w * in_a);
