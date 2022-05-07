92291ca80581a2909cf7e0ebec8372608d3736f2
David Hou <gundamme@gmail.com>
2022-05-07 01:29:24 -0400

    Core input control changes

--- rtl/core_input_ctrl.sv
+++ rtl/core_input_ctrl.sv
@@ -7,7 +7,9 @@ module CORE_INPUT_CTRL #(
     input rstn,
     input [INWIDTH-1 : 0] ainport,
     input [INWIDTH-1 : 0] winport,
-    input r_read,);
+    input r_read,
+    output [INWIDTH-1 : 0] ws [0 : ROWS-1],
+    output [INWIDTH-1 : 0] as [0 : ROWS-1]);
 
     // Instantiate buffers for handling input
     wire abufreads [0 : ROWS-1];
@@ -43,3 +45,48 @@ module CORE_INPUT_CTRL #(
             .dout(bufouts[i]));
     end
     endgenerate
+
+    // counter for scheduling. based on num rows
+    reg [3:0] initcycles;
+
+    // We use initcycles to count whether it is the
+    // initial phases where later rows/columns receive 0s.
+    //               .  .  .  .  .
+    //               W  W  W
+    //               W  W
+    //               W
+    //               V  V  V  V  V
+    //              ________________
+    // ... A A A ->|                |
+    // ... A A   ->|                |
+    // ... A     ->|    PE Array    |
+    // ...       ->|                |
+    // ...       ->|                |
+    //              ________________
+    //
+
+    assign as[0] = ~aemptys[0] ? abufouts[0] : 0;
+    assign ws[0] = ~wemptys[0] ? wbufouts[0] : 0;
+
+    generate
+    for (i=1; i<ROWS; i=i+1) begin
+        assign as[i] = (~aemptys[i] && initcycles >= i) ? abufouts[i] : 0;
+        assign ws[i] = (~wemptys[i] && initcycles >= i) ? wbufouts[i] : 0;
+    end
+    endgenerate
+
+    always @ (posedge clk) begin
+        if (!rstn) begin
+            initcycles <= 0;
+        end else begin
+            if (initcycles < ROWS-1) begin
+                initcycles <= initcycles + 1;
+            end
+
+            // Pending interface to ESP socket
+        end
+    end
+
+
+
+endmodule
