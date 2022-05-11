61c2be28f86ea8d1f34a665454ac60df49f32551
ashment <gundamme@gmail.com>
2022-05-11 03:25:46 -0400

    esp wrapper wip

--- rtl/esp_wrapper.sv
+++ rtl/esp_wrapper.sv
@@ -11,30 +11,30 @@ dma_write_chnl_valid, dma_write_chnl_data, dma_write_chnl_ready);
 
     /* <<--params-def-->> */
     input [31:0]  conf_info_depth;
-    input 	 conf_done;
+    input conf_done;
 
-    input 	 dma_read_ctrl_ready;
-    output 	 dma_read_ctrl_valid;
-    output [31:0] dma_read_ctrl_data_index;
-    output [31:0] dma_read_ctrl_data_length;
-    output [2:0]  dma_read_ctrl_data_size;
+    input dma_read_ctrl_ready;
+    output reg dma_read_ctrl_valid;
+    output reg [31:0] dma_read_ctrl_data_index;
+    output reg [31:0] dma_read_ctrl_data_length;
+    output reg [2:0]  dma_read_ctrl_data_size;
 
-    output 	 dma_read_chnl_ready;
-    input 	 dma_read_chnl_valid;
+    output reg dma_read_chnl_ready;
+    input dma_read_chnl_valid;
     input [31:0]  dma_read_chnl_data;
 
-    input 	 dma_write_ctrl_ready;
-    output 	 dma_write_ctrl_valid;
-    output [31:0] dma_write_ctrl_data_index;
-    output [31:0] dma_write_ctrl_data_length;
-    output [2:0]  dma_write_ctrl_data_size;
+    input dma_write_ctrl_ready;
+    output dma_write_ctrl_valid;
+    output reg [31:0] dma_write_ctrl_data_index;
+    output reg [31:0] dma_write_ctrl_data_length;
+    output reg [2:0]  dma_write_ctrl_data_size;
 
-    input 	 dma_write_chnl_ready;
-    output 	 dma_write_chnl_valid;
-    output [31:0] dma_write_chnl_data;
+    input dma_write_chnl_ready;
+    output reg dma_write_chnl_valid;
+    output reg [31:0] dma_write_chnl_data;
 
-    output 	 acc_done;
-    output [31:0] debug;
+    output reg acc_done;
+    output reg [31:0] debug;
 
     //           A              x         B
     //  _                    _     _             _
@@ -106,16 +106,13 @@ dma_write_chnl_valid, dma_write_chnl_data, dma_write_chnl_ready);
     */
 
     // ESP Socket Outputs
-    reg acc_done;
-
-    // Control Signals
-    reg start;
-    reg done;
+    wire rstn;
+    assign rstn = rst;
 
     // BRAM signals
     reg [10:0] A0, A1;
     reg [7:0] D0, D1;
-    reg [7:0] Q0, Q1;
+    wire [7:0] Q0, Q1;
     reg CE0, CE1, WE0, WE1;
     reg [7:0] WEM0, WEM1;   // This port doesn't appear to do anything
 
@@ -134,9 +131,92 @@ dma_write_chnl_valid, dma_write_chnl_data, dma_write_chnl_ready);
         .CE1(CE1)
     );
 
+    // Number of 64bit beats:
+    // Each beat represents 8x8b data
+    // # of beats per matrix = depth
+
+    // Control Signals
+    reg start;          // Start = 1 after config done.
+    reg [1:0] state;    
+    // STATE:
+    //  00 = loading to plm
+    //  01 = computing
+    //  10 = storing
+    //  11 = done
+    reg [1:0] cnt;      // generic counter for control flow
+    reg [1:0] stateaux; // aux state for internal control flow
+
+    reg [63:0] dmabuf;  // for buffering data from DMA, since its wider than memory
+    reg [31:0] beatit;  // big for counting beat iterations and stuff
+
+    // inptarget: 0 => a | 1 => w
+    reg inptarget;
 
     always @ (posedge clk) begin
-        
+        if (!rstn) begin
+            // RESET
+            start <= 0;
+            state <= 0;
+
+            inptarget <= 0;
+            cnt <= 0;
+            stateaux <= 0;
+            beatit <= 0;
+            
+            // Reset plm con
+            A0 <= 0;    A1 <= 0;
+            D0 <= 0;    D1 <= 0;
+            CE0 <= 0;   CE1 <= 0;
+            WE0 <= 0;   WE1 <= 0;
+            WEM0 <= 0;  WEM1 <= 0;
+
+        end else begin
+            if (start) begin
+                case (state)
+                    2'b00 : begin
+                        // LOAD PLM
+                        // Set read valid and wait for data from DMA
+                        // 00 no read request yet
+                        // 01 waiting for valid data
+                        // 10 writing to valid
+                        case (stateaux)
+                            2'b00 : begin
+                                dma_read_ctrl_valid <= 1;
+                                dma_read_chnl_ready <= 0;
+                                dma_read_ctrl_data_index <= 0;
+                                dma_read_ctrl_data_length <= depth;
+                                dma_read_ctrl_data_size <= 011; //This is token size encoding: 011=64b
+                                dma_read_chnl_ready <= 1;
+
+                                if(dma_read_ctrl_valid && dma_read_ctrl_ready) begin
+                                    dmabuf <= dma_read_chnl_data;
+                                    dma_read_chnl_ready <= 0;   //deasser ready until buffered data written to PLM
+                                    
+                                end
+                            end 
+                            default: 
+                        endcase
+                    end
+
+                    2'b01 : begin
+                        // COMPUTE
+                    end
+                    2'b10 : begin
+                        // Store
+                    end
+                    2'b11 : beginw
+                        acc_done <= 1;
+                    end
+                endcase
+            end else begin
+                if (conf_done) begin
+                    start <= 1;
+                    cnt <= 0;
+                end
+            end
+        end
     end
+
+
 
 endmodule
